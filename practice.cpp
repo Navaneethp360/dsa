4dab91f453b706b6309cebf66e9cb66c660d4211
Navaneethp360 <navaneethp360@gmail.com>
2025-10-14 15:10:11 +0300

    .

--- practice.cpp
+++ practice.cpp
@@ -1200,6 +1200,69 @@ public:
 
 
 
+//merge k sorted lists
+
+class Solution {
+public:
+struct cmp {
+    bool operator()(ListNode*a,ListNode*b)
+{
+    return a->val>b->val;
+}
+};
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,cmp>pq;
+        for(auto l:lists)
+        {
+            if(l)pq.push(l);
+        }
+        ListNode dummy(0);
+        ListNode* tail=&dummy;
+        while(!pq.empty())
+        {
+            ListNode* node=pq.top();
+            pq.pop();
+            tail->next=node;
+            tail=tail->next;
+            if(node->next)pq.push(node->next);
+        }
+        return dummy.next;
+    }
+};
+
+
+struct cmp{
+    bool operator()(ListNode*a,ListNode*b)
+    {
+        return a->val>b->val;
+    }
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,cmp>pq;
+        for(auto l:lists)
+        {
+            if(l).pq.push(l);
+        }
+        ListNode dummy(0);
+        ListNode* tail=&dummy;
+        while(!pq.empty())
+        {
+            ListNode* node=pq.top();
+            pq.pop();
+            tail->next=node;
+            tail=tail->next;
+            if(node->next) pq.push(node->next);
+        }
+        return dummy.next;
+    }
+};
+
+
+
+
+
 A. Arrays / Strings
 
 Two Pointers → e.g. 3Sum, Container With Most Water
